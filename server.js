3fdb953c0af7513f6374853bf571f1f7a4382b63
Patrick Kinsley <102868418+pwkinsley87@users.noreply.github.com>
2022-06-30 09:39:37 -0500

    building callbacks, deleted extra server.js file

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
+const mysql = require('mysql2');
+
 const express = require('express');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -6,15 +9,36 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+//Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        //Your MYSQL username,
+        user: 'pwkinsley87@gmail.com',
+        //Your MySQL password
+        password: 'Aboutthemshoes.',
+        database: 'election'
+    },
 
+    console.log('Connected to election database')
+);
 
-
-
-app.get('/', (req, res) => {
-    res.json({
-        message: 'Hello World!'
-    });
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
 });
+
+// GET a single candidate
+db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(row);
+});
+
+//Default response for any other kind of request (Not Found)
+app.use((req, res) => {
+    res.status(404).end();
+   });
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
